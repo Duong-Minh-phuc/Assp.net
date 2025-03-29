8acceac412bcf23524cb10b04de9026234ad7669
Duong-Minh-phuc <taurus382003@gmail.com>
2025-03-29 10:48:12 +0700

    first commit

---
+++ Example_02/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Example_02.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Example_02.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; } = null!;
+    }
+}
---
+++ Example_02/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Example_02.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string Image { get; set; }
+    }
+}
